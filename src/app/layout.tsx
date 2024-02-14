996e32d1fc894311972ede0366893676bb951472
Temirlan <153745312+temirlannz@users.noreply.github.com>
2024-02-14 19:11:12 +0600

    layot err import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import React from "react";
-import {Boxes} from "@/components/ui/background-boxes";
 
 const inter = Inter({ subsets: ["latin"] });
 
